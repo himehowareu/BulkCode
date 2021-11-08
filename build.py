1842d3f6678a93948674e43f6087493bdf6e9941
Himehowareu <himehowareu@gmail.com>
2021-11-08 11:44:33 -0600

    imporving build script

--- build.py
+++ build.py
@@ -10,6 +10,7 @@ base = "main.js"
 output = "bulkCodes.user.js"
 
 lint = False
+upload = False
 
 def inject(target,token):
     pattern = re.compile(r'\s{2,}|</*himehowareu>')
@@ -22,14 +23,17 @@ def inject(target,token):
         return target
 
 if len(sys.argv) >=2:
+    jshint = os.system('jshint -v 2> nul' if os.name == 'nt' else 'jshint -v > /dev/nul')
+
     if "lint" in sys.argv:
-        jshint = os.system('jshint -v 2> nul' if os.name == 'nt' else 'jshint -v > /dev/nul')
         if jshint == 1:
             print("jshint is not installed can not lint files")
         else:
             lint =True
 
     if "dist" in sys.argv:
+        if not jshint:
+            lint=True
         with open('version.txt') as f:
             current = V(f.read())
         print("current version is " + str(current))
@@ -37,7 +41,14 @@ if len(sys.argv) >=2:
         if new > current:
             with open("version.txt","w") as f:
                 f.write(str(new))
-
+        else:
+            while new < current:
+                print("current version is " + str(current))
+                new = V(input("new version number ? "))
+                if new > current:
+                    with open("version.txt","w") as f:
+                        f.write(str(new))
+        upload = True
 
 with open(output,"w") as outF:
     with open(base,"r") as baseF:
@@ -48,10 +59,20 @@ with open(output,"w") as outF:
             temp = inject(temp,file)
         outF.write(temp)
 
-if lint:
-    out = os.system("jshint javascript")
-    out += os.system("jshint "+output)
-    if out == 0:
-        print("Plugin compiled without error")
+if lint: 
+        out = os.system("jshint javascript")
+        out += os.system("jshint "+output)
+        if out != 0:
+            print("Error in plugin")
+            exit()
+        else:
+            print("Plugin compiled without error")
+
+if upload:
+    git = os.system('git --version 2> nul' if os.name == 'nt' else 'git --version >/dev/nul')
+    if not git:
+        os.system("git add -A")
+        os.system("git commit -am '"+input("commit message: ")+"'")
+        os.system("git push")
     else:
-        print("Error in plugin")
+        print("git is not found , can not upload plugin")
